5b16e60c3242e76dfb469d12e293a882fe56cf10
Roman Antonov <romwtb@gmail.com>
2017-10-19 00:54:06 +0700

    Photo, active link, techs

--- app/src/App.js
+++ app/src/App.js
@@ -2,75 +2,37 @@ import React, { Component } from 'react';
 
 import './App.css';
 
-class Summary extends Component {
-  
-  constructor(props) {
-    super(props);
-    this.summary = this.props.summary;
-    //handle Clicks
-    this.removeTechnology = this.removeTechnology.bind(this);
-  }
-
-  removeTechnology(item) {
-    for (var i=0;i<this.summary.length;i++) {
-      if(item===this.summary[i]) this.summary.splice(i,1)
-    }
-    this.props.onSummaryChange(this.summary);
-  }
-
-  render () {
-    return (
-      <div className="Summary">
-          <div className="Categories">
-            <h2>Summary</h2>
-          </div>
-          <div className="Technologies">
-           {
-                this.summary.map((technology) =>
-               
-                (<div onClick={() => this.removeTechnology(technology)} className="Technology" key={technology.title}>
-                  <div className="tech-image">
-                    <img src={technology.img} alt=""/>
-                  </div>
-                  <div className="title">
-                    {technology.title}
-                  </div>
-                </div>))
-                
-            }
-            </div>
-      </div>
-    );
-  }
-}
-
 class Tech extends Component {
   constructor(props) {
     super(props);
-    this.state = {categories: [], technologies: [], currentCategory: 0};
-    this.summary = this.props.summary;
+    this.state = {categories: [], technologies: [], currentCategory: 0, active_item: []};
     //handle Clicks
     this.chooseCategory = this.chooseCategory.bind(this);
-    this.pickTechnology = this.pickTechnology.bind(this);
   }
-
+  
   componentDidMount() {
     var This = this
     fetch('technologies.json').then(function(response) {
       return response.json();
     }).then(function(json){
-       This.setState({'categories':json.categories,'technologies':json.technologies});
+       let items = []
+       for (var i = 0; i < json.categories.length; i ++) {
+         items.push(false)
+       }
+       items[0] = true
+       This.setState({'categories':json.categories,'technologies':json.technologies, active_item: items});
     }).catch(function(error) {
       console.log('parsing failed', error)
     })
   }
 
   chooseCategory(id) {
-    this.setState({'currentCategory':id})
-  }
-  pickTechnology(item) {
-    this.summary.push(item)
-    this.props.onSummaryChange(this.summary);
+    let items = []
+    for (var i = 0; i < this.state.active_item.length; i ++) {
+      items.push(false)
+    }
+    items[id] = true
+    this.setState({currentCategory:id, active_item:items})
   }
 
   render () {
@@ -81,14 +43,20 @@ class Tech extends Component {
           <div className="Categories">
             {
               categories.map((category) =>
-              <div onClick={() => this.chooseCategory(category.id)} className="category" key={category.id}>{category.title}</div>)
+              <div onClick={() => this.chooseCategory(category.id)} 
+                   className="category" 
+                   key={category.id}>
+                <span className={this.state.active_item[category.id] ? 'active': null} >
+                  {category.title}
+                </span>
+              </div>)
             }
           </div>
           <div className="Technologies">
               {
                 technologies.map((technology) =>
                 (technology.category === this.state.currentCategory) ? (
-                <div onClick={() => this.pickTechnology(technology)} className="Technology" key={technology.title}>
+                <div className="Technology" key={technology.title}>
                   <div className="tech-image">
                     <img src={technology.img} alt=""/>
                   </div>
@@ -106,35 +74,25 @@ class Tech extends Component {
 
 
 class App extends Component {
-  
-  constructor(props) {
-    super(props);
-    this.state = {
-      summary:[]
-    };
-    //example bind event to elment
-    this.onSummaryChange = this.onSummaryChange.bind(this);
-  }
-
-  onSummaryChange(summary){
-    this.setState({
-      summary: summary
-    });
-  }
 
   render() {
     return (
       <div className="App">
         <div id="About">
+          <img alt="Roman Antonov" className="avatar" src="https://odesk-prod-portraits.s3.amazonaws.com/Users:withoutb:PortraitUrl_100?AWSAccessKeyId=1XVAX3FNQZAFC9GJCFR2&Expires=2147483647&Signature=vMLlACdKXb9cR1rFZvbjlJlc2jo%3D&1496145669596530&8flr8"/>
           <h1>Roman Antonov</h1>
           <h2>Modern Software developing</h2>
           <h3>
-            <a href="https://upwork.com/fl/romanantonov" target="_blank">
+            <a href="https://upwork.com/fl/romanantonov" 
+               rel="noopener noreferrer" 
+               target="_blank">
               Upwork Top Rated
             </a>
           </h3>
           <h3>
-            <a href="https://www.upwork.com/signup/create-account/client_contact_freelancer?ciphertext=~016b923b0158ef81ae&BYOC" target="_blank">
+            <a href="https://www.upwork.com/signup/create-account/client_contact_freelancer?ciphertext=~016b923b0158ef81ae&BYOC"
+               target="_blank" 
+               rel="noopener noreferrer">
               Hire me
             </a>
           </h3>
@@ -157,6 +115,7 @@ class App extends Component {
           Most popular services
           <br />
           <ul>
+            <li>Facebook messenger chat bot</li>
             <li>Code assessment and review</li>
             <li>Build cloud server architecture</li>
             <li>Real estate property management system, sync with airbnb</li>
@@ -171,18 +130,18 @@ class App extends Component {
           Fixed price deals can be covered by paypal transfers or swift transfer services.
         </div>
 
-        <Tech summary={this.state.summary} onSummaryChange={this.onSummaryChange} />
-        {/* <Summary summary={this.state.summary} onSummaryChange={this.onSummaryChange} /> */}
+        <Tech />
+
         <div id="Contacts">
-          Feel free to ask questions.
-          <br />
-          I will happy to consult you!
-          <br />
           E-mail : romwtb@gmail.com
           <br />
           Skype: romwtb@gmail.com
           <br />
-          Upwork: <a href="https://upwork.com/fl/romanantonov" target="_blank">https://upwork.com/fl/romanantonov</a>
+          Upwork: <a href="https://upwork.com/fl/romanantonov" 
+                     rel="noopener noreferrer"
+                     target="_blank">
+                     https://upwork.com/fl/romanantonov
+                  </a>
         </div>
       </div>
     );
