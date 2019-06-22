1d7ff5132c40b161bdada9728a74d53b988f1b77
roman-rr <romwtb@gmail..com>
2019-06-22 15:51:02 +0700

    checker

--- marketing-app/main.js
+++ marketing-app/main.js
@@ -1026,6 +1026,7 @@ var HomePage = /** @class */ (function () {
         this.activatedRoute.queryParams.subscribe(function (params) {
             _this.id = params['id'] || 1;
         });
+        alert('NEW');
     }
     HomePage.prototype.goNext = function () {
         // if (!this.goalReached) {
