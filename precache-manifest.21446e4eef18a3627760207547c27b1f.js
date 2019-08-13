375a1c0d90a8f1f371738358a3aabe1c27484133
roman-rr <romwtb@gmail.com>
2019-08-13 18:58:31 +0700

    Updates

---
+++ precache-manifest.21446e4eef18a3627760207547c27b1f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbe5638c73a3d29973d96693c0ffa495",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53df6dff59b01d480537",
+    "url": "/static/css/main.0949a7b5.chunk.css"
+  },
+  {
+    "revision": "6385f5c411b16529d235",
+    "url": "/static/js/2.d6992a11.chunk.js"
+  },
+  {
+    "revision": "53df6dff59b01d480537",
+    "url": "/static/js/main.9fd88197.chunk.js"
+  },
+  {
+    "revision": "8d73872ef07c9f7ffb5a",
+    "url": "/static/js/runtime~main.37144203.js"
+  }
+]);
